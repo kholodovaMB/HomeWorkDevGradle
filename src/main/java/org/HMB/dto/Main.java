8772e6554350ab3eb67080697598ab249c038607
kholodovaMB <holodova.maryna@gmail.com>
2023-04-29 16:54:23 +0300

    ignore idea

--- src/main/java/org/HMB/dto/Main.java
+++ src/main/java/org/HMB/dto/Main.java
@@ -1,7 +1,27 @@
-package org.HMB;
+package org.HMB.dto;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class Main {
+    private static final String FILE_NAME = "./src/main/resources/user.json";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .serializeNulls()
+                .create();
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(FILE_NAME);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        UserDto dto = gson.fromJson(fileReader, UserDto.class);
+        System.out.println("------------");
+        System.out.println(dto.toString());
     }
 }
--- src/main/java/org/HMB/dto/UserDto.java
+++ src/main/java/org/HMB/dto/UserDto.java
@@ -1,2 +1,22 @@
-package org.HMB.dto;public class UserDto {
+package org.HMB.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+
+public class UserDto {
+    private String name;
+    private String lastName;
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
